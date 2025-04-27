c79af9067e11ce117d94ee9a768e78a698538d7d
ankitsihag2003 <ankitsihag2003@gmail.com>
2025-04-27 17:51:09 +0530

    updated generate token

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,7 +4,7 @@ export const generateToken = (res, user, message) => {
     const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' });
     return res
         .status(200)
-        .cookie("token", token, { httpOnly: true, sameSite: "strict", maxAge: 24 * 60 * 60 * 1000 })
+        .cookie("token", token, { httpOnly: true, sameSite: "None",secure:true, maxAge: 24 * 60 * 60 * 1000 })
         .json({
             success:true,
             message:message,
