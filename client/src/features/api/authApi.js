5d9b909d7d2b3c294aef1e407b31a4682bc197ce
ankitsihag2003 <157290589+ankitsihag2003@users.noreply.github.com>
2025-04-27 20:46:44 +0530

    Update authApi.js

--- client/src/features/api/authApi.js
+++ client/src/features/api/authApi.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import { userLoggedIn, userLoggedOut } from "../authSlice";
 
-const User_api = "https://lms-app-backend-qmqx.onrender.com/api/v1/user/";
+const User_api = "https://lms-app-backend-rf2y.onrender.com/api/v1/user/";
 
 export const authApi = createApi({
     reducerPath:"authApi",
