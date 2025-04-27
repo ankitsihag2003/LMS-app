d590761e3d8e6040bf62f68d63c4a3064f2c7559
ankitsihag2003 <157290589+ankitsihag2003@users.noreply.github.com>
2025-04-27 20:06:03 +0530

    Update purchaseApi.js

--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const base_api = "http://localhost:8080/api/v1/purchase/";
+const base_api = "https://lms-app-backend-qmqx.onrender.com/purchase/";
 
 export const purchaseApi = createApi({
     reducerPath: "purchaseApi",
