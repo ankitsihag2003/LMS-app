58f6f8537edf997b1565797f55a4580fdb52c128
ankitsihag2003 <157290589+ankitsihag2003@users.noreply.github.com>
2025-04-27 10:37:00 +0530

    Update courseApi.js

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const course_api = 'http://localhost:8080/api/v1/course';
+const course_api = 'https://lms-app-backend-etbb.onrender.com/api/v1/course';
 
 export const courseApi = createApi({
     reducerPath:'courseApi',
