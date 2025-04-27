e9f56f96b48579384f50d47973d381cb7014afe0
ankitsihag2003 <157290589+ankitsihag2003@users.noreply.github.com>
2025-04-27 10:57:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ connectDB()
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://lms-app-frontend.onrender.com",
     credentials:true
 }))
 
