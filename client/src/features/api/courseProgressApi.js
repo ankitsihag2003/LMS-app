373c37917787e22cdc034890cbc63528c8605293
ankitsihag2003 <157290589+ankitsihag2003@users.noreply.github.com>
2025-04-27 20:33:55 +0530

    Update courseProgressApi.js

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const base_api = "https://lms-app-backend-qmqx.onrender.com/progress";
+const base_api = "https://lms-app-backend-qmqx.onrender.com/api/v1/progress";
 
 export const progressApi = createApi({
     reducerPath: "progressApi",
