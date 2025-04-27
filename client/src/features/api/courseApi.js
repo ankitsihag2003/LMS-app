f7b14105306aad179cc16b4ab05903eef20909f4
ankitsihag2003 <157290589+ankitsihag2003@users.noreply.github.com>
2025-04-27 20:05:04 +0530

    Update courseApi.js

--- client/src/features/api/courseApi.js
+++ client/src/features/api/courseApi.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const course_api = 'http://localhost:8080/api/v1/course';
+const course_api = 'https://lms-app-backend-qmqx.onrender.com/course';
 
 export const courseApi = createApi({
     reducerPath:'courseApi',
